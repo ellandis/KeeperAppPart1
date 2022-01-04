b6ec930408b062ea563bada12b6c8782f8596039
ellandis <ellandis@crimson.ua.edu>
2022-01-04 10:03:20 -0500

    React Components
    Please install node modules to run

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="note">
+            <h1 >The is the Title</h1>
+            <p >The paragraph content</p>
+        </div>
+    );
+}
+
+export default Note;
